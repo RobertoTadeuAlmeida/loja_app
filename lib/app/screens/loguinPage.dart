70b7ff636cca3c3539ee3c71f6db6ca34bd2daa4
unknown <robertotadeudealmeida@gmail.com>
2024-06-03 16:12:15 -0300

    validação do campo de email

--- lib/app/screens/loguinPage.dart
+++ lib/app/screens/loguinPage.dart
@@ -9,33 +9,65 @@ class LoguinPage extends StatefulWidget {
 }
 
 class _LoguinPageState extends State<LoguinPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                style: new TextStyle(color: Colors.white, fontSize: 20),
-                decoration: getAuthenticationInputDecoration('E-mail'),
-              ),
-              Divider(),
-              TextFormField(
-                obscureText: true,
-                style: new TextStyle(color: Colors.white, fontSize: 20),
-                decoration: getAuthenticationInputDecoration('Senha'),
-              ),
-              Divider(),
-              ElevatedButton(onPressed: () {}, child: Text("Entrar"))
-            ],
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  controller: _emailController,
+                  validator: (String? value) {
+                    if (value == null) {
+                      return 'Preencha o E-mail por favor.';
+                    }
+                    if (value.length < 5) {
+                      return "E-mail muito curto.";
+                    }
+                    if (!value.contains("@")) {
+                      return "E-mail invalido";
+                    }
+                    return null;
+                  },
+                  style: new TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: getAuthenticationInputDecoration('E-mail'),
+                ),
+                Divider(),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  style: new TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: getAuthenticationInputDecoration('Senha'),
+                ),
+                Divider(),
+                ElevatedButton(onPressed: () {
+                  loguinPageButtonClick();
+                }, child: Text("Entrar"))
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  loguinPageButtonClick() {
+    if (_formKey.currentState!.validate()) {
+      print("Form valido");
+    }else {
+      print("Form invalido");
+    }
   }
 }
